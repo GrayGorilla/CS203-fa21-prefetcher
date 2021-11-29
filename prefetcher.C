d88f880c7e4a5dd5c69508b8a0a5702f0c7e1ee5
GrayGorilla <nbren004@ucr.edu>
2021-11-29 03:23:33 -0800

    implemented sequential prefetcher to always prefetch the next address

--- prefetcher.C
+++ prefetcher.C
@@ -7,10 +7,15 @@ bool Prefetcher::hasRequest(u_int32_t cycle) { return false; }
 
 Request Prefetcher::getRequest(u_int32_t cycle) {
 	Request req;
-	req.addr = 0;
+	req.addr = nextReqAddr;
 	return req;
 }
 
 void Prefetcher::completeRequest(u_int32_t cycle) { isReady = false; }
 
-void Prefetcher::cpuRequest(Request req) { return; }
+void Prefetcher::cpuRequest(Request req) {
+	if (!isReady) {
+		nextReqAddr = req.addr + 16;
+		isReady = true;
+	}
+}
